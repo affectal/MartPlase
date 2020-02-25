1cb74efe3eead8a53cc61ad5acebab97fa1e7529
affectal <aff.p@yandex.ru>
2020-02-25 22:21:11 +0700

    app/index.html

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,18 @@ $(function() {
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         dots: false,
         arrows: true,
-        rows: '0'
+
+    });
+
+
+    $('.followers__slider').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        arrows: true,
+
     });
 
 
